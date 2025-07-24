729da11b262c99574ea375bf8cb2af53530994ba
awais6132 <151523161+awais6132@users.noreply.github.com>
2025-07-24 08:57:20 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "awaisali.pixector@gmail.com"
+var password = "123456"
+accountCity = "Lahore"
+let accountState ;
+
+
+console.log (accountId);
+console.log(accountEmail);
+console.log(password);
+console.log(accountCity);
+console.table([accountId , accountEmail , accountCity , password , accountState]);
