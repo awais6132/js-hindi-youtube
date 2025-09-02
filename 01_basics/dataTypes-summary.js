ef7ce0cc4e4ee91427845dd60b62d56a36487a10
awais6132 <151523161+awais6132@users.noreply.github.com>
2025-09-02 17:43:18 +0000

    dataTypes-summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,29 @@
+// primitive
+
+//  7 types : String , Nmber , Boolean , null , undefined , Symbol , BigInt
+
+const score = false
+// In type Script
+//const score1:number = 100 
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // undefined
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+
+// References (Non Primitive)
+
+// Array , Object , Function 
+
+const heors = ["quaid" , "Aqbal" , "zia"]
+ let myObj ={
+    name : "awais",
+    age : 28 ,
+
+ }
+
+ const myFunction = function(){
+    console.log("hello world");
+ } 
